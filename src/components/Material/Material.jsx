849183bde3171de8b77c21ed6e7cb35c0e4ac6d0
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-05-25 04:11:02 +0300

    tmp

--- src/components/Material/Material.jsx
+++ src/components/Material/Material.jsx
@@ -1,22 +1,61 @@
-export const Material = ({ item, onDelete, onUpdate }) => {
-  const { id, title, link } = item;
+import { Component } from 'react';
 
+const EditMaterialModal = ({ onClose, onEdit }) => {
   return (
-    <>
-      <p>
-        <b>Title:</b> {title}
-      </p>
-      <p>
-        <b>Link:</b> {link}
-      </p>
-
-      <button type="button" onClick={() => onUpdate({ id, title: Date.now() })}>
-        Edit
+    <div>
+      <h2>Модалка редактирования материала</h2>
+      <button
+        type="button"
+        onClick={() => {
+          onEdit();
+          onClose();
+        }}
+      >
+        Вот теперь точно редактировать
       </button>
-
-      <button type="button" onClick={() => onDelete(id)}>
-        Delete
+      <button type="button" onClick={onClose}>
+        Close
       </button>
-    </>
+    </div>
   );
 };
+
+// $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+export class Material extends Component {
+  state = {
+    isModalOpen: false,
+  };
+
+  openModal = () => this.setState({ isModalOpen: true });
+  closeModal = () => this.setState({ isModalOpen: false });
+
+  render() {
+    const { item, onUpdate, onDelete } = this.props;
+    const { isModalOpen } = this.state;
+    const { id, title, link } = item;
+
+    return (
+      <div>
+        <p>
+          <b>Title:</b> {title}
+        </p>
+        <p>
+          <b>Link:</b> {link}
+        </p>
+        <button type="button" onClick={() => onDelete(id)}>
+          Delete
+        </button>
+        <button type="button" onClick={this.openModal}>
+          Edit
+        </button>
+        {isModalOpen && (
+          <EditMaterialModal
+            onClose={this.closeModal}
+            onEdit={() => onUpdate({ id, title: Date.now() })}
+          />
+        )}
+      </div>
+    );
+  }
+}
